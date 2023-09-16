0ae2304d8153eed77297c724608d738df90e3e57
Alisson Cavalcanti Galvão <alisson.galvao@amcom.com.br>
2023-09-16 07:21:11 -0300

    Implements getList

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -5,6 +5,7 @@ const jimp = require('jimp');
 const Ad = require('../models/Ad');
 const Category = require('../models/Category');
 const User = require('../models/User');
+const StateModel = require('../models/State');
 
 const addImage = async(buffer) => {
     let newName = `${uuid()}.jpg`;
@@ -92,8 +93,67 @@ module.exports = {
         return;
 
     },
+
     getList: async(req, res) => {
-        return true;
+        //q filter = parametro de pesquisa (palavra)
+        let { sort = 'asc', offset = 0, limit = 8, q, cat, state } = req.query;
+        let filters = { status: true }
+            //total de registros para paginação
+        let total = 0;
+
+        //filtros
+        if (q) {
+            //usando regex com opção case "insesitive"
+            filters.title = { '$regex': q, '$options': 'i' };
+        }
+        if (cat) {
+            const c = await Category.findOne({ slug: cat }).exec();
+            if (c) {
+                filters.category = c._id.toString();
+            }
+        }
+        if (state) {
+            const s = await StateModel.findOne({ name: state.toUpperCase() }).exec();
+            if (s) {
+                filters.state = s._id.toString();
+            }
+        }
+
+        //consultando o total de registros
+        const adsTotal = await Ad.find(filters).exec();
+        total = adsTotal.length;
+
+
+        //montagem da consulta
+        const adsData = await Ad.find(filters)
+            .sort({ dateCreated: (sort == 'desc' ? -1 : 1) })
+            .skip(parseInt(offset))
+            .limit(parseInt(limit))
+            .exec();
+
+        let ads = [];
+
+        for (let i in adsData) {
+
+            let image;
+            let defaultImg = adsData[i].images.find(e => e.default);
+            if (defaultImg) {
+                image = `${process.env.BASE}/media/${defaultImg.url}`;
+            } else {
+                image = `${process.env.BASE}/media/default.jpg`;
+            }
+
+            ads.push({
+                id: adsData[i]._id,
+                title: adsData[i].title,
+                price: adsData[i].price,
+                priceNegotiable: adsData[i].priceNegotiable,
+                image
+            });
+        }
+
+        res.json({ ads, total });
+        return;
     },
     getItem: async(req, res) => {
         return true;
